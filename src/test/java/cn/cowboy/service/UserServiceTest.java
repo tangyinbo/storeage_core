e7853d24e4d1aeec9c5cbe57be3423e315b6686a
canton_cowboy <tangyinboit@163.com>
2014-06-11 09:03:17 +0800

    test

--- src/test/java/cn/cowboy/service/UserServiceTest.java
+++ src/test/java/cn/cowboy/service/UserServiceTest.java
@@ -1,23 +1,23 @@
 package cn.cowboy.service;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import cn.cowboy.domain.User;
 import cn.cowboy.parent.BaseTest;
 
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"classpath:spring.xml","classpath:spring-mybatis.xml"})
-public class UserServiceTest{
+public class UserServiceTest extends BaseTest{
 	@Autowired
 	private UserService userService;
 	
 	@Test
 	public void test1(){
-		System.out.println("========>>>"+userService);
+		User user = new User();
+		user.setUserName("tangyinbo2");
+		user.setPassword("123456");
+		user.setSalt(System.currentTimeMillis()+"");
+		user.setStat("S0A");
+		userService.createUser(user);
 	}
 }
