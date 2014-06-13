26977909b42d2bda43696d6fc7a9f5ae6a76b8a1
canton_cowboy <tangyinboit@163.com>
2014-06-13 17:42:37 +0800

    shiro form 登录初步完成2 ,commit the missing file

---
+++ src/main/java/cn/cowboy/provide/constants/Constants.java
@@ -0,0 +1,5 @@
+package cn.cowboy.provide.constants;
+
+public class Constants {
+	 public static final String CURRENT_USER = "user";
+}
---
+++ src/main/java/cn/cowboy/shiro/filter/SysUserFilter.java
@@ -0,0 +1,25 @@
+package cn.cowboy.shiro.filter;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.web.filter.PathMatchingFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import cn.cowboy.provide.constants.Constants;
+import cn.cowboy.service.UserService;
+
+public class SysUserFilter extends PathMatchingFilter {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    protected boolean onPreHandle(ServletRequest request, ServletResponse response, Object mappedValue) throws Exception {
+
+        String username = (String)SecurityUtils.getSubject().getPrincipal();
+        request.setAttribute(Constants.CURRENT_USER, userService.findByUsername(username));
+        return true;
+    }
+}
